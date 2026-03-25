81e8c4411c28c41c14f28eaeb13684e65538de70
Huy <huyb2205870@student.ctu.edu.vn>
2026-03-25 14:53:42 +0700

    Add example config and Flyway migration

--- backend/api/src/main/java/com/social/SocialNetworkApplication.java
+++ backend/api/src/main/java/com/social/SocialNetworkApplication.java
@@ -2,8 +2,10 @@ package com.social;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.social")
 public class SocialNetworkApplication {
 
     public static void main(String[] args) {
